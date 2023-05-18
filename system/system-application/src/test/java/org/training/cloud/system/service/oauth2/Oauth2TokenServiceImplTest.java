5c3a87b62d87dedeac32e2a0de2cd5c8ec18a5e0
王统周 <wangtongzhou520@163.com>
2023-05-19 07:55:19 +0800

    Nacos配置文件测试

--- system/system-application/src/test/java/org/training/cloud/system/service/oauth2/Oauth2TokenServiceImplTest.java
+++ system/system-application/src/test/java/org/training/cloud/system/service/oauth2/Oauth2TokenServiceImplTest.java
@@ -3,7 +3,9 @@ package org.training.cloud.system.service.oauth2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.training.cloud.system.entity.oauth2.SysOauth2AccessToken;
 
 import java.util.Objects;
@@ -21,9 +23,12 @@ public class Oauth2TokenServiceImplTest {
     @Autowired
     private Oauth2TokenService oauth2TokenService;
 
+    @Value("${system.database.name}")
+    private String systemDatabaseName;
+
     @Test
     public void queryAccessTokenByAccessToken() {
-
+        String name= systemDatabaseName;
         SysOauth2AccessToken sysOauth2AccessToken =
                 oauth2TokenService.queryAccessTokenByAccessToken("94b8278b-e904-4231-bd19-5b571e4db7dc");
         Assertions.assertTrue(Objects.nonNull(sysOauth2AccessToken));
